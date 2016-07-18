16f55e1b8262cf2e1a4ad1a3230e4524f4d7cf8c
amingilani <aminshahgilani@gmail.com>
2016-07-19 03:48:06 +0500

    Improve language of the response

--- mail-counter.js
+++ mail-counter.js
@@ -21,10 +21,20 @@ module.exports = function(ctx, cb) {
             if (!data) data = {};
             if (!data.counter) data.counter = 0;
 
+            switch (data.counter) {
+                case 0:
+                    response = "Hey Diego, the mail has not been read yet.";
+                    break;
+                case 1:
+                    response = "Hey Diego, the mail was read once.";
+                    break;
+                default:
+                    response = "Hey Diego, the mail was read " +
+                        data.counter +
+                        " times";
+            }
+
             // return how many times the email has been viewed
-            response = "Hey Diego, the mail was read " +
-            data.counter +
-            " times";
             cb(null, response);
         });
     }
